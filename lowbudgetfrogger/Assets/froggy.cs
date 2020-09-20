c9d79ff671546817815d7d3c858342103ef97b43
Julian <Julian.frost101@gmail.com>
2020-09-20 19:02:15 -0400

    added cubes and code to made cubes reset after move frog movement to (no reset though?)

---
+++ lowbudgetfrogger/Assets/froggy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class froggy : MonoBehaviour
+   
+    
+    
+{
+
+    Transform myTransform;
+    public float speed = 2.24f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            myTransform.position += new Vector3(0, Time.deltaTime*speed, 0);
+        }
+
+        
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            myTransform.position += new Vector3(0, -Time.deltaTime * speed, 0);
+        }
+
+
+
+
+
+    }
+}
---
+++ lowbudgetfrogger/Assets/movecar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class movecar: MonoBehaviour
+{
+    public Transform myTransform;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(1f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position = myTransform.position + new Vector3(1f, 0, 0) * speed * Time.deltaTime;
+
+
+        if(myTransform.position.x> 5)
+        {
+            myTransform.position += Vector3.left * 10;
+        }
+
+
+
+
+    }
+}
